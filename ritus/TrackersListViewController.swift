d36a574fd9075ce8bd08efbe1f0d423885e13bf9
Mariia Mohylska <mariia.mohylska@gmail.com>
2024-04-17 19:52:11 -0400

    Added main navigation

--- ritus/TrackersListViewController.swift
+++ ritus/TrackersListViewController.swift
@@ -11,7 +11,7 @@ class TrackersListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tabBarController?.selectedIndex = 0
         // Do any additional setup after loading the view.
     }
     
